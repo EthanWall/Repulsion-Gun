22befd4c7d9e75b00545416c73110da208cba16d
EthanWall <53667981+EthanWall@users.noreply.github.com>
2019-11-22 18:19:23 -0500

    Added basic item pickup system

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public GameObject target;
+    public float smoothing = 1.0f;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * smoothing);
+
+        Quaternion rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;
+    new Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = item.GetComponent<Rigidbody>();
+
+        rigidbody.useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        rigidbody.useGravity = false;
+        rigidbody.isKinematic = true;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = tempParent.transform;
+    }
+
+    void OnMouseUp()
+    {
+        rigidbody.useGravity = true;
+        rigidbody.isKinematic = false;
+        item.transform.parent = null;
+        item.transform.position = guide.transform.position;
+    }
+}
